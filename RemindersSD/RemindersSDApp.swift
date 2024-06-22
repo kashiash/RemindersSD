b715e13074ac7f02728a108cc77b73a4ac5fe552
Jacek Kosinski <kashiash@gmail.com>
2024-06-22 11:49:23 +0200

    Initial Commit

---
+++ RemindersSD/RemindersSDApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersSDApp.swift
+//  RemindersSD
+//
+//  Created by Jacek Kosinski U on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RemindersSDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
