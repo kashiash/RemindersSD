723a10d94393f7772c4e2e4d8c1920aa7e72e5ac
Jacek Kosinski <kashiash@gmail.com>
2024-06-25 00:23:03 +0200

    new reminder alert

--- RemindersSD/Screens/MyListDetailScreen.swift
+++ RemindersSD/Screens/MyListDetailScreen.swift
@@ -11,13 +11,42 @@ import SwiftData
 
 struct MyListDetailScreen: View {
     @State  var myList: MyList
+
+    @State private var title: String = ""
+    @State private var isSheetPresented = false
+
     var body: some View {
         VStack {
             List(myList.reminders) { reminder in
                 Text(reminder.title)
             }
+            Spacer()
+            Button(action: {
+                isSheetPresented.toggle()
+            }, label :{
+                HStack{
+                    Image(systemName: "plus.circle.fill")
+                    Text("New reminder")
+                }
+            })
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding()
         }
         .navigationTitle(myList.name)
+       // .navigationBarTitleDisplayMode(.inline)
+        .alert("New reminder", isPresented: $isSheetPresented) {
+            TextField("",text: $title)
+            Button("Cancel",role: .cancel) {}
+            Button("Done") {
+                saveReminder()
+            }
+        }
+    }
+
+    func saveReminder() {
+        let reminder = Reminder(title: title)
+        myList.reminders.append(reminder)
+        title = ""
     }
 }
 
